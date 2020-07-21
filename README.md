d4c3d5c984f1dd5a05763cdbc9e0f79cdb739d7f
rvignav <rvignav@gmail.com>
2020-07-20 21:12:28 -0700

    Add main.py

---
+++ README.md
@@ -0,0 +1,10 @@
+# CT2Xray
+
+To test the CT to Xray algorithm, run the following:
+
+    git clone https://github.com/rvignav/CT2Xray.git
+    cd CT2Xray
+    pip install Pillow numpy glob2 regex
+    python main.py
+
+Note: If `import glob` still raises an error, run `npm install glob`.
---
+++ main.py
@@ -0,0 +1,54 @@
+from PIL import Image
+import numpy as np
+import glob
+import re
+
+images = glob.glob("CT/*.png")
+
+def sort_list( l ):
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key = alphanum_key)
+
+# Sort images alphanumerically by filename to ensure that z loops from front to back
+images = sort_list(images)
+
+# Handle images of different sizes
+m = np.asarray(Image.open(images[0])).shape[1]
+for i in range(len(images)):
+    m = max(m, np.asarray(Image.open(images[i])).shape[1])
+p = np.zeros((len(images), m, 3))
+
+# Loop through CT slices from front to back
+for z in range(len(images)):
+    image = Image.open(images[z])
+    pixels = np.asarray(image)
+    if len(pixels.shape) == 2:	
+        # Loop from left to right on the CT slice
+        for x in range(pixels.shape[1]):
+            # Sum y values in the current x column
+            sum = 0
+            for y in range(pixels.shape[0]):
+                sum += pixels[y][x]
+            # Assign sum to the point (x, z) on the coronal image - p[z][x] in the pixel array, 
+            # since z represents height (rows) and x represents length (columns)
+            p[z][x] = sum / pixels.shape[0]
+    else:
+        # Remove alpha channel
+        if pixels.shape[2] == 4:
+            pixels = pixels[:,:,:-1]	
+        for x in range(pixels.shape[1]):
+            sum = np.zeros(pixels.shape[2])
+            for y in range(pixels.shape[0]):
+                new = np.zeros(pixels.shape[2])
+                for i in range(pixels.shape[2]):
+                    new[i] = pixels[y][x][i] + sum[i]
+                sum = new
+            p[z][x] = sum
+            
+# Save and display image
+array = np.array(p, dtype=np.uint8)
+xray = Image.fromarray(array)
+xray.save('xray.png')
+print('Xray saved to \'xray.png\'')
+xray.show()
