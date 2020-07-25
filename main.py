7be04493b643a91dec8ca78f9eec4ceb9a676f40
rvignav <rvignav@gmail.com>
2020-07-25 16:54:42 -0700

    Fix test script

--- main.py
+++ main.py
@@ -18,13 +18,18 @@ def sort_list(l):
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
     return sorted(l, key = alphanum_key)
 
-def create_image(dirname, label):
+def create_image(dirname, dirname2, label):
     if dirname[-1] == "/":
         dirname = dirname[:-1]
     images = glob.glob(str(dirname) + "/*.png")
 
+    if dirname2[-1] == "/":
+        dirname2 = dirname2[:-1]
+    images2 = glob.glob(str(dirname2) + "/*.png")
+
     # Sort images alphanumerically by filename to ensure that z loops from front to back
     images = sort_list(images)
+    images2 = sort_list(images2)
 
     p = np.zeros((len(images), Image.open(images[0]).convert('L').size[0]))
 
@@ -32,7 +37,7 @@ def create_image(dirname, label):
     toolbar_width = int(len(images)/5 + 1)
     sys.stdout.write(label + " [%s]" % (" " * toolbar_width))
     sys.stdout.flush()
-    sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+    sys.stdout.write("\b" * (toolbar_width+1))
 
     # Loop through CT slices from front to back
     for z in range(len(images)):
@@ -41,6 +46,15 @@ def create_image(dirname, label):
         data = list(img.getdata()) # convert image data to a list of integers
         # convert that to 2D list (list of lists of integers)
         pixels = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
+
+        img2 = Image.open(images2[z]).convert('L') 
+        data2 = list(img2.getdata()) 
+        pixels2 = [data2[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
+
+        for r in range(HEIGHT):
+            for c in range(WIDTH):
+                if pixels2[r][c] != 0:
+                    pixels[r][c] = pixels2[r][c]
 
         # Loop from left to right on the CT slice
         for x in range(WIDTH):
@@ -58,26 +72,18 @@ def create_image(dirname, label):
             sys.stdout.flush()
     sys.stdout.write("]\n")
 
-    return p
+    plt.imshow(p, cmap='gray')
+    plt.gca().set_axis_off()
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
+                hspace = 0, wspace = 0)
+    plt.margins(0,0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.savefig('xray.png', bbox_inches = 'tight',
+        pad_inches = 0)
 
 # Save and display image
-xray = create_image(args.CT_dir, "Creating X-ray:")
-mask = create_image(args.mask_dir, "Creating mask: ")
-
-for r in range(xray.shape[0]):
-    for c in range(xray.shape[1]):
-        if mask[r][c] != 0:
-            xray[r][c] = mask[r][c]
-
-plt.imshow(xray, cmap='gray')
-plt.gca().set_axis_off()
-plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
-            hspace = 0, wspace = 0)
-plt.margins(0,0)
-plt.gca().xaxis.set_major_locator(plt.NullLocator())
-plt.gca().yaxis.set_major_locator(plt.NullLocator())
-plt.savefig('xray.png', bbox_inches = 'tight',
-    pad_inches = 0)
+create_image(args.CT_dir, args.mask_dir, "Creating X-ray:")
 
 final_img = Image.open('xray.png')
 size = 300
