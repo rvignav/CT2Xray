707dbe621fc04e6e386a4dde9059fc0b5c0c433a
rvignav <rvignav@gmail.com>
2020-12-06 22:20:11 -0800

    Update lesion

--- validation/hand_seg/resize.py
+++
@@ -1,8 +0,0 @@
-import glob
-from PIL import Image
-i = 0
-for f in glob.glob("../images/*"):
-    img = Image.open(f)
-    img = img.resize((432,300))
-    img.save('../images/vx' + str(i) + '.png')
-    i += 1
