ac2fdd7b89d8e6401f562a62cd70679f10979df4
rvignav <rvignav@gmail.com>
2021-04-01 20:45:31 -0700

    Train with 60/40 train-test split

--- general_blockchain/compute_dice.py
+++ general_blockchain/compute_dice.py
@@ -5,11 +5,11 @@ import scipy.stats
 import matplotlib.pyplot as plt 
 
 d = glob.glob('/Users/vignavramesh/Documents/CT2Xray/tests/ground_truth_masks/*')
-# d2 = glob.glob('/Users/vignavramesh/Documents/CT2Xray/tests/xrays_only_masks/*')
+d2 = glob.glob('/Users/vignavramesh/Documents/CT2Xray/tests/xrays_only_masks/*')
 d3 = glob.glob('/Users/vignavramesh/Documents/CT2Xray/tests/mixed_masks/*')
 
 d = sorted(d)
-# d2 = sorted(d2)
+d2 = sorted(d2)
 d3 = sorted(d3)
 
 dices = []
@@ -22,11 +22,11 @@ def replace_last(source_string, replace_what, replace_with):
 for i in range(len(d3)):
     im3 = Image.open(d3[i]).convert('L')
     im1n = '/Users/vignavramesh/Documents/CT2Xray/tests/ground_truth_masks/' + str(d3[i][d3[i].rindex("mixed_masks/")+len("mixed_masks/"):])
-    # im2n = '/Users/vignavramesh/Documents/CT2Xray/tests/xrays_only_masks/' + str(d3[i][d3[i].rindex("mixed_masks/")+ len("mixed_masks/"):])
+    im2n = '/Users/vignavramesh/Documents/CT2Xray/tests/xrays_only_masks/' + str(d3[i][d3[i].rindex("mixed_masks/")+ len("mixed_masks/"):])
     im1n = replace_last(im1n, 'jpeg', 'png')
     try:
         im1 = Image.open(im1n).convert('L').resize((1024,1024))
-        # im2 = Image.open(im2n).convert('L')
+        im2 = Image.open(im2n).convert('L')
     except:
         print(d3[i][d3[i].rindex("mixed_masks/")+ len("mixed_masks/"):])
 
@@ -34,8 +34,8 @@ for i in range(len(d3)):
     data = list(im1.getdata())
     pixels = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
 
-    # data2 = list(im2.getdata())
-    # pixels2 = [data2[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
+    data2 = list(im2.getdata())
+    pixels2 = [data2[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
     
     data3 = list(im3.getdata())
     pixels3 = [data3[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
@@ -47,8 +47,8 @@ for i in range(len(d3)):
     for r in range(len(pixels)):
         for c in range(len(pixels[r])):
             u += 1
-            # if (pixels[r][c] == pixels2[r][c]):
-            #     n1 += 1
+            if (pixels[r][c] == pixels2[r][c]):
+                n1 += 1
             if (pixels[r][c] == pixels3[r][c]):
                 n2 += 1
 
